c54f348daf0a2db79a34c6dee520dfa2f418bdea
Brent Spooner <brent@jakray.com>
2013-07-08 23:55:45 +0000

    revert to orig web.js

--- web.js
+++ web.js
@@ -3,9 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var indexHtml=new indexHtml;
-  indexHtml =fs.readFileSync('index.html',"utf-8");
-  response.send(indexHtml);
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
